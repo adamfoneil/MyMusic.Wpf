7b5d09b4414ba5af9535dfee3be2e9ec434dddbe
Sabapathy <37434203+sabapathygithub@users.noreply.github.com>
2021-07-11 07:31:56 +0530

    Issue 5 - View file location context menu.

--- MyMusic.Wpf/Controls/Playlist.xaml.cs
+++ MyMusic.Wpf/Controls/Playlist.xaml.cs
@@ -122,6 +122,10 @@ namespace MyMusic.Wpf.Controls
             Mp3FileSelected?.Invoke(mp3file);
         }
 
-        private void viewFileLocation_Click(object sender, RoutedEventArgs e) => FileUtil.RevealInExplorer(((sender as DataGrid).CurrentItem as Mp3File).FullPath);        
+        private void viewFileLocation_Click(object sender, RoutedEventArgs e)
+        {
+            FileUtil.RevealInExplorer((CurrentTrack as Mp3File).FullPath);
+        }
+                
     }
 }
